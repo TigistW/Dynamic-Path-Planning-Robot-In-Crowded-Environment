2d8a4f698de96c198042f0a13803d4cacf535580
Nahom32 <senaynahom00@gmail.com>
2025-01-22 12:54:04 +0300

    chore: removed some files

--- src/build/dynamic_planner/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/nahom/Dynamic-Path-Planning-Robot-In-Crowded-Environment/src/install/dynamic_planner'
